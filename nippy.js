e59323b1aba43fb35bebb3ef1cec9149869d7a51
1Nippy <38084692+1Nippy@users.noreply.github.com>
2018-05-15 12:23:39 +0200

    Update nippy.js

--- nippy.js
+++ nippy.js
@@ -236,6 +236,8 @@ if (msg.startsWith(prefix + 'help')){ //here comes the prefix which is assigned 
     .addField("`Creator`", `***Hello, I am <@366609578625859584>, and I am a small bot made by <@262347242252402698>***`) //adds a field with the following content
     .addBlankField()//Blank field for more beautiful display
     .addField("`Invite Link`", "*Add me to your server with the link given to you with this command! -Invite*") //adds a field with the following content
+    .addBlankField()
+    .addField("`Prefix`", "*The prefix for this bot is set to* _** - **_")
     .addField("`Word Replacements`", "*The following words get replaced with Pictures/Gifs:*\u200b```Hey, Hello, Bye, Shut up, Hackerman, Help, Invite, Sah dude, Awesome, Yes, No, Kys, Kms, Earth, Wanker, Special, Sad, Hype, Heil, haha, no u, Ily, Fuck, Thinking, Hentai, Anime, xd, Spoopy, Heyyy, dd, skrrt, Gay, Dead inside, Limbo, Bounce, Faggot, toxic ```\u200b*Use: Place requested word inbetween 2 Minus': -word-*")
     .addBlankField()//Blank field for more beautiful display
     .setTimestamp() //This is at the bottom of the embed and shows when the Embed was created. You can delete this if you don't want it
