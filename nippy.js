2fe54d04506421a66d144901071f5f92f54f9c14
1Nippy <38084692+1Nippy@users.noreply.github.com>
2018-06-07 12:04:10 +0200

    Update nippy.js

--- nippy.js
+++ nippy.js
@@ -310,7 +310,7 @@ if (msg.startsWith(prefix + 'help')){ //here comes the prefix which is assigned 
     .addField("`Prefix`", "*The prefix for this bot is set to* _** - **_")
     .addField("`Word Replacements`", "*The following words get replaced with Pictures/Gifs:*\u200b```Hey, Hello, Bye, Shut up, Hackerman, Help, Invite, Sah dude, Awesome, Yes, No, Kys, Kms, Earth, Wanker, Special, Sad, Hype, Heil, haha, no u, Ily, Fuck, Thinking, Hentai, Anime, xd, Spoopy, Heyyy, dd, skrrt, Gay, Dead inside, Limbo, Bounce, Faggot, Toxic, Table Flip, Ball, Surf, Bipolar, Winner, Nohomo, Cock ```\u200b*Use: Place requested word inbetween 2 Minus': -word-*")
     .addBlankField()
-    .addField("`R6S Operators`", "*Some of the operators from Rainbow Six | Siege works as word replacements, feel free to try them out*)
+    .addField("`R6S Operators`", "*Some of the operators from Rainbow Six | Siege works as word replacements, feel free to try them out*")
     .addBlankField()//Blank field for more beautiful display
     .setTimestamp() //This is at the bottom of the embed and shows when the Embed was created. You can delete this if you don't want it
      message.channel.send({embed}); //sends EMBED, this is needed, otherwise it won't create the embed
